5c366cbdae1f85c6efca32a2fccdca7f62134b9b
JohnJessieD <144754347+JohnJessieD@users.noreply.github.com>
2023-11-29 16:09:06 +0800

    2

--- capstone/src/components/Sidebar.vue
+++ capstone/src/components/Sidebar.vue
@@ -4,20 +4,18 @@
       <v-navigation-drawer :mini-variant.sync="isCollapsed" app>
         <v-list dense>
           <v-list-item-group>
-            
             <v-list-item v-for="link in links" :key="link.to" @click="navigate(link.to)" :class="{ 'link-active': isActive(link.to) }">
               <v-list-item-action>
                 <v-icon>{{ link.icon }}</v-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>{{ link.text }}</v-list-item-title>
-                
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
           <v-btn icon @click="logout">
-          <v-icon>mdi-logout</v-icon>
-        </v-btn>
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
         </v-list>
       </v-navigation-drawer>
   
@@ -30,38 +28,41 @@
   
       <!-- Main Content -->
       <v-main>
-
+        <!-- Dynamically load component based on active link -->
+        <router-view :key="$route.path"></router-view>
       </v-main>
     </v-app>
   </template>
-<script>
-export default {
-  data() {
-    return {
-      
-      isCollapsed: false,
-      links: [
-        { to: '/Dashboard', icon: 'mdi-view-dashboard', text: 'Dashboard' },
-        { to: '/Offices', icon: 'mdi-office-building', text: 'Offices' },
-        { to: '/Data', icon: 'mdi-database', text: 'Data' },
-        { to: '/User', icon: 'mdi-account', text: 'User' },
-      ],
-    };
-  },
-  methods: {
-    toggleCollapse() {
-      this.isCollapsed = !this.isCollapsed;
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isCollapsed: false,
+        links: [
+          { to: '/Dashboard', icon: 'mdi-view-dashboard', text: 'Dashboard' },
+          { to: '/Offices', icon: 'mdi-office-building', text: 'Offices' },
+          { to: '/Data', icon: 'mdi-database', text: 'Data' },
+          { to: '/User', icon: 'mdi-account', text: 'User' },
+        ],
+      };
     },
-    navigate(to) {
-      this.$router.push(to);
-    },
-    isActive(route) {
-      return this.$route.path === route;
-    },
-    logout() {
-      // Perform any additional logout logic (clear session, etc.) here
-      // For example, you can use localStorage to clear the user session
-      localStorage.removeItem('userToken');this.$router.push('/')
+    methods: {
+      toggleCollapse() {
+        this.isCollapsed = !this.isCollapsed;
+      },
+      navigate(to) {
+        // Update the route to trigger the dynamic component loading
+        this.$router.push(to);
+      },
+      isActive(route) {
+        return this.$route.path === route;
+      },
+      logout() {
+        // Perform any additional logout logic (clear session, etc.) here
+        // For example, you can use localStorage to clear the user session
+        localStorage.removeItem('userToken');
+        this.$router.push('/');
       },
     },
   };
@@ -71,12 +72,13 @@ export default {
   .link-active {
     background-color: #e0e0e0 !important; /* Highlight active link with a background color */
   }
-
+  
   .v-navigation-drawer {
     transition: min-width 0.3s !important;
   }
-
+  
   .v-navigation-drawer--open {
     min-width: 300px !important; /* Set the desired open width */
   }
-</style>
+  </style>
+  
---
+++ capstone/src/views/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header />
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  // Components
+  import Header from '../components/Header.vue';
+  
+  export default defineComponent({
+    name: 'HomeView',
+  
+    components: {
+      Header,
+    },
+  });
+  </script>
+  
